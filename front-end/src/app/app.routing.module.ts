604b47a68a52a9b291fbfc8f900a794bc6f0f54f
ronaldo <ronaldo.luizcp@gmail.com>
2021-11-20 17:28:56 -0300

    Subindo componente de tela de adm ja com a rota

--- front-end/src/app/app.routing.module.ts
+++ front-end/src/app/app.routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { AdministradorComponent } from './administrador/administrador.component';
 import { ContatoComponent } from './contato/contato.component';
 import { CortesComponent } from './cortes/cortes.component';
 import { HomeComponent } from './home/home.component';
@@ -26,6 +27,10 @@ const routes: Routes = [
     path: 'contato',
     component: ContatoComponent
   },
+  {
+    path: 'adm',
+    component: AdministradorComponent
+  }
 ];
 
 @NgModule({
